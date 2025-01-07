4e428f503981a561c7adab0f88ee2c7e290997f8
hasina samuel <rafalimananahasina.ext@orange.com>
2025-01-07 17:24:30 +0300

    creation composant

--- snapface/src/app/face-snap/face-snap.component.ts
+++ snapface/src/app/face-snap/face-snap.component.ts
@@ -12,17 +12,37 @@ export class FaceSnapComponent implements OnInit{
   imageUrl!: string;
   description!: string;
   createdAt!: Date;
-  snaps!: number
+  snaps!: number;
+  snapButtonText!: string;
+  userHasSnapped!: boolean;
 
   ngOnInit(): void{
     this.title = "Mes image";
     this.imageUrl = "https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg"
     this.description = "Liste des mes images"
     this.createdAt = new Date();
-    this.snaps = 5
+    this.snaps = 0;
+    this.snapButtonText = "Oh snap";
+    this.userHasSnapped = false
   }
 
-  onAddSnap(): void {
-    this.snaps++;
+  onSnap(): void {
+    if(!this.userHasSnapped){
+      this.snap()
+  }else{
+    this.unSnap()
   }
+}
+
+snap(): void {
+  this.snaps++;
+  this.snapButtonText = "Oops, un Snap!";
+  this.userHasSnapped = true
+}
+
+unSnap(): void {
+  this.snaps--;
+  this.snapButtonText = "Oh snap";
+  this.userHasSnapped = false
+}
 }
