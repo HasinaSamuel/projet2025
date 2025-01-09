2db115063ad72cbfbac751e47ec282941807706f
hasina samuel <rafalimananahasina.ext@orange.com>
2025-01-09 17:06:32 +0300

    create directiv

--- snapface/src/app/app.component.ts
+++ snapface/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component} from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { SnapFaceListComponent } from './snap-face-list/snap-face-list.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [SnapFaceListComponent,HeaderComponent],
+  imports: [HeaderComponent, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- snapface/src/app/app.routes.ts
+++ snapface/src/app/app.routes.ts
@@ -1,3 +1,9 @@
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
+import { SnapFaceListComponent } from './snap-face-list/snap-face-list.component';
+export const routes: Routes = [
+  {path:'face-snaps', component: SnapFaceListComponent},
+  {path: '', component: LandingPageComponent}
 
-export const routes: Routes = [];
+];
